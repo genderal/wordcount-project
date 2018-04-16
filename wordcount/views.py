bdae3ed150d9725a736b499a42067906320539b7
genderal <eng.mhassan88@gmail.com>
2018-04-16 19:32:31 +0200

    home page with about

---
+++ wordcount/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def home(request):
+    return render(request,'home.html')
+def count(request):
+    text=request.GET['fulltext']
+    wordlist=text.split()
+    worddictionary={}
+    for word in wordlist:
+        if word in worddictionary:
+            worddictionary[word]+=1
+        else:
+            worddictionary[word]=1
+    listedwords= sorted(worddictionary.items(),key=operator.itemgetter(1),reverse=True)
+
+    return render(request,'count.html',{'fulltext':text,'count':len(wordlist),'worddictionary':listedwords})
+def about(request):
+    return render(request,'about.html')
